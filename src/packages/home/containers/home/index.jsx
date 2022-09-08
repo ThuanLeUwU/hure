30a4824efd9fb98b32daedc945af063dd3b3a02b
BuildTools <kainesv86@gmail.com>
2022-09-08 17:19:15 +0700

    merging

--- src/packages/home/containers/home/index.jsx
+++ src/packages/home/containers/home/index.jsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import { Navbar } from '../../../../core/components/navbar';
 import { Banner } from '../../../../core/components/banner';
 import { OurMissionVision } from '../../../../core/components/OurMissionVision';
 
 export const Home = () => {
     return (
         <div>
-            <Navbar />
             <Banner />
             <OurMissionVision />
         </div>
