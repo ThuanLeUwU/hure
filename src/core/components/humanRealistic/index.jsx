96089a6c1b3f102d588acbdacd62d4d6acec3aa2
thuan <lthuan2809@gmail.com>
2022-09-08 16:59:57 +0700

    HumanRealistic-update

---
+++ src/core/components/humanRealistic/index.jsx
@@ -0,0 +1,55 @@
+// import * as React from 'react';
+// import HumanRealisticStyles from './styles/HumanRealistic.module.scss';
+
+// export const HumanRealistic = () => {
+//     return (
+//         <section className={`${HumanRealisticStyles.section}`}>
+//             <div className={`${HumanRealisticStyles.container}`}>
+//                 <div className={`${HumanRealisticStyles.wrapper}`}>
+//                     <h3 className={`${HumanRealisticStyles.human_realistic_platform}`}>Human Realistic Platform</h3>
+//                     <p className={`${HumanRealisticStyles.human_realistic_platform_content}`}>
+//                         HuRe platform will provide a destination for human model system tech to live, interact and flourish
+//                     </p>
+//                 </div>
+//                 <div className={`${HumanRealisticStyles.row}`}>
+//                     <div>
+//                         {/* <Video
+//   borderRadius={16} 
+//   srcFile={MmohB3jdeCoJt2WDgx0K1JAirvA.mp4}
+//   srcType="Upload"
+//   // Using default values:
+//   backgroundColor="rgba(0, 0, 0, 0)"
+//   canvasPlay={false}
+//   controls={false}
+//   loop={true}
+//   muted={true}
+//   objectFit="cover"
+//   onClick={undefined}
+//   onEnd={undefined}
+//   onMouseEnter={undefined}
+//   onMouseLeave={undefined}
+//   onPause={undefined}
+//   onPlay={undefined}
+//   onSeeked={undefined}
+//   playing={true}
+//   poster={undefined}
+//   posterEnabled={false}
+//   srcUrl="https://assets.mixkit.co/videos/preview/mixkit-ice-cream-glass-of-red-soda-5094-small.mp4"
+//   startTime={0}
+//   volume={25}
+// /> */}
+//                     </div>
+
+//                     <div className={`${HumanRealisticStyles.frame}`}>
+//                         <div className={`${HumanRealisticStyles.image}`}>
+//                             <div className={`${HumanRealisticStyles.frame_content}`}></div>
+//                         </div>
+//                         {/* <div className={`${HumanRealisticStyles.frame_content}`}>
+//                             <div className={`${HumanRealisticStyles.image}`}></div>
+//                         </div> */}
+//                     </div>
+//                 </div>
+//             </div>
+//         </section>
+//     );
+// };
--- src/packages/home/containers/home/index.jsx
+++ src/packages/home/containers/home/index.jsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
+import HumanRealistic from '../../../../core/components/humanRealistic';
+// import {HumanRealistic} from '../../../../core/components/humanRealistic';
 import { Navbar } from '../../../../core/components/navbar';
 
 export const Home = () => {
     return (
         <div>
             <Navbar />
+            <HumanRealistic/>
         </div>
     );
 };
