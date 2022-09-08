1e6e9d9f8812365e9459dcbd1f40470cbaac2937
PhamNgocThien1801 <phamngocthien.dev@gmail.com>
2022-09-08 16:56:36 +0700

    out mission & vision

--- src/packages/home/containers/home/index.jsx
+++ src/packages/home/containers/home/index.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Navbar } from '../../../../core/components/navbar';
+import { OurMissionVision } from '../../../../core/components/OurMissionVision';
 
 export const Home = () => {
     return (
         <div>
             <Navbar />
+            <OurMissionVision />
         </div>
     );
 };
