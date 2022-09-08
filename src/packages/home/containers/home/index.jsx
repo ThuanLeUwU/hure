eb43dba4db92b57bb4870b48aa3e50c3708f8dd3
MeowMeowKit <92076631+MeowMeowKit@users.noreply.github.com>
2022-09-08 15:38:16 +0700

    Section Banner

--- src/packages/home/containers/home/index.jsx
+++ src/packages/home/containers/home/index.jsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { Navbar } from '../../../../core/components/navbar';
+import { Banner } from '../../../../core/components/banner';
 
 export const Home = () => {
     return (
         <div>
             <Navbar />
+            <Banner />
         </div>
     );
 };
